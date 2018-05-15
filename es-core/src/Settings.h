f2dea190c18acf0878f8cd5bca6b94119441b22f
S3rious Sam <verrsam@hotmail.com>
2018-05-15 13:36:18 -0400

    Implements SettingsV2...

--- es-core/src/Settings.h
+++ es-core/src/Settings.h
@@ -31,3 +31,19 @@ private:
 	std::map<std::string, float> mFloatMap;
 	std::map<std::string, std::string> mStringMap;
 };
+
+namespace SettingsV2
+{
+    void saveFile();
+
+    // You get a warning if you try a get on a key that is not already present.
+    bool getBool(const std::string& name);
+    int getInt(const std::string& name);
+    float getFloat(const std::string& name);
+    const std::string& getString(const std::string& name);
+
+    void setBool(const std::string& name, bool value);
+    void setInt(const std::string& name, int value);
+    void setFloat(const std::string& name, float value);
+    void setString(const std::string& name, const std::string& value);
+}
